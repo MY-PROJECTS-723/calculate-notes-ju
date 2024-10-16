855b1a4d1ff1862fe4bb7d20c6f1a0d6594c2807
SebasCorrea23 <juansebastiancorreagarcia@gmail.com>
2024-10-16 08:33:23 -0500

    Cambio Guardado

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ const username = document.getElementById('name');
   const btnCalculate = document.getElementById('btn-calculate');
   const predict= document.getElementById('btn-predict');
   const response = document.getElementById('resultado');
+  const btnPredict = document.getElementById('btn-predict');
 
   btnCalculate.addEventListener('click', calculateNote);
+  btnPredict.addEventListener('click', predictNote);
 
-  function calculateNote() {
+  
+  function calculateNote(event) {
     event.preventDefault();
     let user= username.value
     let note1 = Number(data1.value);
@@ -30,12 +33,33 @@ const username = document.getElementById('name');
         response.style.color='green';
     }
     else {
-         response.textContent=`Hola ${user} alguna de sus notas es invalida,revise`;
-    }
-
-    // Function predict note
-    
-    function predictNote(){
-        let note
+         response.textContent=`Hola ${user} alguna de sus notas es invalida, revise`;
     }
   }
+
+    // Function predict note
+
+  function predictNote(event){
+    event.preventDefault();
+    let user = username.value;
+    let note1 = Number(data1.value);
+    let note2 = Number(data2.value);
+   
+    //Nota deseada
+    let notaDeseada = 3.6;
+
+    let predict = (notaDeseada - (note1 * 0.3) - (note2 * 0.3)) / 0.4.toFixed(2);
+
+    if(note1 < 1 && note1 > 5 && note2 < 1 && note2 > 5){
+       alert('Alguna nota no concuerda');
+       return;
+    }
+
+    if(predict > 5){
+        response.textContent = `Hola ${user} te falta: ${predict.toFixed(2)} para pasar la materia`
+    } else if( predict === 3.5){
+        response.textContent = `Hola ${user} con las notas actuales, ya ha alcanzado una nota definitiva mayor a 3,5`;
+    }else{
+        response.textContent = `Hola ${user}, necesitas al menos: ${predict.toFixed(2)} para pasar la materia`;
+    }
+}
